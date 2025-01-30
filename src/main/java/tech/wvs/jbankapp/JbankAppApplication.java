786143bbc7a8a01f686a5745637933ccb20d74be
wallacevilela7 <wallace.vilela9@gmail.com>
2025-01-30 16:16:27 -0300

    initialize jbank-app with Maven setup and basic application structure

---
+++ src/main/java/tech/wvs/jbankapp/JbankAppApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.jbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbankAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/jbankapp/JbankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.jbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
