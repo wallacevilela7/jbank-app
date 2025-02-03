9d13411eb858fa65c30be3d0ababd3704365a2df
wallacevilela7 <wallace.vilela9@gmail.com>
2025-02-03 15:23:34 -0300

    add AuditInterceptor and InterceptorConfig for request auditing

---
+++ src/main/java/tech/wvs/jbankapp/interceptors/AuditInterceptor.java
@@ -0,0 +1,41 @@
+package tech.wvs.jbankapp.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class AuditInterceptor implements HandlerInterceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(AuditInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex) throws Exception {
+        logger.info("Audit - Method: {}, Url: {}, StatusCode: {}, IpAddress: {} ",
+                request.getMethod(),
+                request.getRequestURI(),
+                response.getStatus(),
+                request.getAttribute("x-user-ip"));
+    }
+}
---
+++ src/main/java/tech/wvs/jbankapp/interceptors/InterceptorConfig.java
@@ -0,0 +1,21 @@
+package tech.wvs.jbankapp.interceptors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final AuditInterceptor auditInterceptor;
+
+    public InterceptorConfig(AuditInterceptor auditInterceptor) {
+        this.auditInterceptor = auditInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(auditInterceptor)
+                .order(0);
+    }
+}
