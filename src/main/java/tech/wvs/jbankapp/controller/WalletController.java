9d5edeb821c61b0253ce97aa3cc6422084678f9d
wallacevilela7 <wallace.vilela9@gmail.com>
2025-02-03 18:56:40 -0300

    add exception handling in WalletController (basic way)

--- src/main/java/tech/wvs/jbankapp/controller/WalletController.java
+++ src/main/java/tech/wvs/jbankapp/controller/WalletController.java
@@ -1,11 +1,11 @@
 package tech.wvs.jbankapp.controller;
 
+import org.springframework.beans.factory.parsing.Problem;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.wvs.jbankapp.controller.dto.CreateWalletDto;
+import tech.wvs.jbankapp.exception.WalletDataAlreadyExistsException;
 import tech.wvs.jbankapp.service.WalletService;
 
 import java.net.URI;
@@ -23,5 +23,15 @@ public class WalletController {
     public ResponseEntity<Void> createWallet(@RequestBody CreateWalletDto data) {
         var wallet = walletService.createWallet(data);
         return ResponseEntity.created(URI.create("/wallets/" + wallet.getWalletId().toString())).build();
+    }
+
+    @ExceptionHandler(WalletDataAlreadyExistsException.class)
+    public ProblemDetail handleWalletDataAlreadyExistsException(WalletDataAlreadyExistsException e) {
+        var pd  = ProblemDetail.forStatus(422);
+
+        pd.setTitle("Wallet data already exists");
+        pd.setDetail(e.getMessage());
+
+        return pd;
     }
 }
